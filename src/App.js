a9eff113c93d008c4ba88e943acb152b22947bb1
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-02-11 09:04:46 -0500

    editing snapshot to function properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  // grabbing name off of profile
+  const [gitHubName, setGitHubName] = useState('')     
+  // grabbing the url of account
+  const [gitHubURL, setGitHubURL] = useState('')   
+  // For Bonus grabbing an avatar img
+  const [gitHubImageURL, setGitHubImageURL] = useState('')              
+  // grabbing bio from the profile page
+  const [gitHubBio, setGitHubBio] = useState('')
+
+  useEffect(() => {
+      fetch('https://api.github.com/users/dvogel345')
+      .then(res => res.json())
+      .then(data => {
+        setGitHubName(data.name)
+        setGitHubURL(data.html_url)
+        setGitHubImageURL(data.avatar_url)
+        setGitHubBio(data.bio)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
+        <a href={gitHubURL}><button>Link to GitHub profile</button></a>
+      </div>
+      {/* For Bonus */}
+      <div className="pt-5">
+        <img src={gitHubImageURL} alt='Github profile image' width='200' height='200'/>
+        <p>{gitHubBio}</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+import renderer from 'react-test-renderer'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+beforeEach(() => {
+  // sets everything back to initial state after each test
+  fetch.resetMocks();
+})
+
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+// describe is a jest method that contains one or more related tests.
+describe('receives values from GitHub REST API using jest fetch mock', () => {
+  test("receives GitHub name", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Derek Vogel'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('Derek Vogel')
+  })
+
+  test("receives GitHub URL", async () => {
+    fetch.mockResponseOnce(JSON.stringify({html_url: 'https://github.com/dvogel345'}))
+    render(<App />)
+    const gitHubURL = await waitFor(() => screen.getByRole('link'))
+    expect(gitHubURL).toHaveAttribute('href', expect.stringContaining('github.com'))
+  })
+
+  // For Bonus
+  // grabbing image from profile
+  test("receives GitHub Image URL", async () => {
+    fetch.mockResponseOnce(JSON.stringify({avatar_url: 'https://avatars.githubusercontent.com/u/78238679?v=4'}))
+    render(<App />)
+    const gitHubURL = await waitFor(() => screen.getByAltText('Github profile image'))
+    expect(gitHubURL).toHaveAttribute('src', expect.stringContaining('githubusercontent'))
+  })
+
+  // grabbing bio from the profile
+  test("recives GitHub Bio from profile", async () => {
+    fetch.mockResponseOnce(JSON.stringify({bio: "Student with Thrive DX partnered with NJIT, Remote\r\nFull-Stack Web Developer Bootcamp"}))
+    render(<App />)
+    const gitHubBio = await waitFor(() => screen.getAllByRole('p'))
+    expect(gitHubBio).toHaveTextContent("Student with Thrive DX partnered with NJIT, Remote\r\nFull-Stack Web Developer Bootcamp")
+  })
+})
+
